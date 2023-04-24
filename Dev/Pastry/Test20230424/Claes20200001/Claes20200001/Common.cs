bab95e61aa6a1c7350a46703334d8149ef73776f
stackprobe <stackprobes@gmail.com>
2023-04-24 15:50:46 +0900

    Voyager 1 is 23896672354.457 kilometers away from the Sun.

--- Dev/Pastry/Test20230424/Claes20200001/Claes20200001/Common.cs
+++ Dev/Pastry/Test20230424/Claes20200001/Claes20200001/Common.cs
@@ -7,6 +7,9 @@ namespace Charlotte
 {
 	public static class Common
 	{
-		// none
+		public static bool IsMbcKana(char chr)
+		{
+			return 0x30a1 <= (int)chr && (int)chr <= 0x30fc; // ? カタカナ(30A1～30FA) || 中点(30FB) || 長音記号(30FC)
+		}
 	}
 }

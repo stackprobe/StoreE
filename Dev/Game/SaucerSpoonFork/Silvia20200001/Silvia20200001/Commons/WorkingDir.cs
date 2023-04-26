fffb8fcfb6b7a15f4cf30c2f10986870146acc7a
stackprobe <stackprobes@gmail.com>
2023-04-26 15:14:03 +0900

    Voyager 1 is 23899553835.100 kilometers away from the Sun.

---
+++ Dev/Game/SaucerSpoonFork/Silvia20200001/Silvia20200001/Commons/WorkingDir.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Diagnostics;
+
+namespace Charlotte.Commons
+{
+	public class WorkingDir : IDisposable
+	{
+		public static RootInfo Root = null;
+
+		public class RootInfo
+		{
+			private string Dir = null;
+
+			public string GetDir()
+			{
+				if (this.Dir == null)
+				{
+					string dir = GetRootDir();
+
+					SCommon.DeletePath(dir);
+					SCommon.CreateDir(dir);
+
+					this.Dir = dir;
+				}
+				return this.Dir;
+			}
+
+			public void Delete()
+			{
+				if (this.Dir != null)
+				{
+					try
+					{
+						Directory.Delete(this.Dir, true);
+					}
+					catch (Exception e)
+					{
+						ProcMain.WriteLog(e);
+					}
+
+					this.Dir = null;
+				}
+			}
+		}
+
+		private static string GetRootDir()
+		{
+			string envTMP = Environment.GetEnvironmentVariable("TMP");
+
+			if (string.IsNullOrEmpty(envTMP))
+				throw new Exception("Environment variable TMP is not defined");
+
+			return Path.Combine(envTMP, "Claes20200001_TMP_{683426cc-d32b-485d-ad69-c4f210938f72}_" + Process.GetCurrentProcess().Id);
+		}
+
+		private static ulong CtorCounter = 0;
+
+		private string Dir = null;
+
+		private string GetDir()
+		{
+			if (this.Dir == null)
+			{
+				if (Root == null)
+					throw new Exception("Root is null");
+
+				this.Dir = Path.Combine(Root.GetDir(), (CtorCounter++).ToString("x16"));
+
+				SCommon.CreateDir(this.Dir);
+			}
+			return this.Dir;
+		}
+
+		public string GetPath(string localName)
+		{
+			return Path.Combine(this.GetDir(), localName);
+		}
+
+		private ulong PathCounter = 0;
+
+		public string MakePath()
+		{
+			return this.GetPath((this.PathCounter++).ToString("x16"));
+		}
+
+		public void Dispose()
+		{
+			if (this.Dir != null)
+			{
+				try
+				{
+					Directory.Delete(this.Dir, true);
+				}
+				catch (Exception e)
+				{
+					ProcMain.WriteLog(e);
+				}
+
+				this.Dir = null;
+			}
+		}
+	}
+}

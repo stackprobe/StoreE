f678408e6d950796d2ae854ebbbc2281f1c80f37
stackprobe <stackprobes@gmail.com>
2023-04-29 10:22:11 +0900

    Voyager 1 is 23903636070.058 kilometers away from the Sun.

--- Dev/Program/Cron/Release.bat
+++ Dev/Program/Cron/Release.bat
@@ -1,7 +1,7 @@
 CALL C:\Factory\SetEnv.bat
 CALL Clean.bat
 cx **
-IF ERRORLEVEL 1 START *[BUILD-ERROR]
+IF ERRORLEVEL 1 C:\app\MsgBox\MsgBox.exe E "BUILD ERROR"
 
 acp Petra20200001\Program.exe out\*P.exe
 xcp doc out

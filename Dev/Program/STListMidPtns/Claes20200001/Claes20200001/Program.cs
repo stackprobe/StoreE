8e739c78b659217e3c817df8c3f31686d41f8876
stackprobe <stackprobes@gmail.com>
2023-04-30 15:15:58 +0900

    Voyager 1 is 23905393186.813 kilometers away from the Sun.

--- Dev/Program/STListMidPtns/Claes20200001/Claes20200001/Program.cs
+++ Dev/Program/STListMidPtns/Claes20200001/Claes20200001/Program.cs
@@ -18,52 +18,6 @@ namespace Charlotte
 		{
 			ProcMain.CUIMain(new Program().Main2);
 		}
-
-		// 以下様式統一のため用途別に好きな方を使ってね -- ★要削除
-
-#if true // 主にデバッガで実行するテスト用プログラム -- ★不要なら要削除
-		private void Main2(ArgsReader ar)
-		{
-			if (ProcMain.DEBUG)
-			{
-				Main3();
-			}
-			else
-			{
-				Main4();
-			}
-			SCommon.OpenOutputDirIfCreated();
-		}
-
-		private void Main3()
-		{
-			Main4();
-			SCommon.Pause();
-		}
-
-		private void Main4()
-		{
-			try
-			{
-				Main5();
-			}
-			catch (Exception ex)
-			{
-				ProcMain.WriteLog(ex);
-			}
-		}
-
-		private void Main5()
-		{
-			// -- choose one --
-
-			new Test0001().Test01();
-			//new Test0002().Test01();
-			//new Test0003().Test01();
-
-			// --
-		}
-#else // 主に実行ファイルにして使う/コマンド引数有り -- ★不要なら要削除
 		private void Main2(ArgsReader ar)
 		{
 			if (ProcMain.DEBUG)
@@ -83,7 +37,7 @@ namespace Charlotte
 #if DEBUG
 			// -- choose one --
 
-			Main4(new ArgsReader(new string[] { }));
+			Main4(new ArgsReader(new string[] { @"C:\temp\Data", @"C:\temp\List.txt", "ABC", "DEF", "GHI" }));
 			//new Test0001().Test01();
 			//new Test0002().Test01();
 			//new Test0003().Test01();
@@ -103,7 +57,7 @@ namespace Charlotte
 			{
 				ProcMain.WriteLog(ex);
 
-				//MessageBox.Show("" + ex, Path.GetFileNameWithoutExtension(ProcMain.SelfFile) + " / エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+				MessageBox.Show("" + ex, Path.GetFileNameWithoutExtension(ProcMain.SelfFile) + " / エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
 
 				//Console.WriteLine("Press ENTER key. (エラーによりプログラムを終了します)");
 				//Console.ReadLine();
@@ -112,8 +66,38 @@ namespace Charlotte
 
 		private void Main5(ArgsReader ar)
 		{
-			// TODO
+			string dir = SCommon.MakeFullPath(ar.NextArg());
+			string destFile = SCommon.MakeFullPath(ar.NextArg());
+			string[] midPtns = ar.TrailArgs().ToArray();
+
+			ProcMain.WriteLog("< " + dir);
+			ProcMain.WriteLog("> " + destFile);
+			ProcMain.WriteLog("P " + string.Join(" OR ", midPtns));
+
+			if (!Directory.Exists(dir))
+				throw new Exception("no dir");
+
+			if (Directory.Exists(destFile))
+				throw new Exception("Bad destFile");
+
+			if (midPtns.Length == 0)
+				throw new Exception("no midPtns");
+
+			if (midPtns.Any(midPtn => string.IsNullOrWhiteSpace(midPtn)))
+				throw new Exception("Bad midPtns");
+
+			string[] destLines = Directory.GetFiles(dir)
+				.Where(file => midPtns.Any(midPtn => SCommon.ContainsIgnoreCase(Path.GetFileName(file), midPtn)))
+				.OrderBy(SCommon.CompIgnoreCase)
+				.ToArray();
+
+			File.WriteAllLines(
+				destFile,
+				destLines,
+				SCommon.ENCODING_SJIS
+				);
+
+			ProcMain.WriteLog("done!");
 		}
-#endif
 	}
 }
